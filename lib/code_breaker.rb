b0c28d8cf5270ed66feb6e5e174ea40947e9c7d4
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-17 18:10:13 +0100

    Minor

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -1,19 +1,16 @@
 class CodeBreaker
-    def initialize
-      code_maker = CodeMaker.new
-      @colors = code_maker.colors
-    end
+  def initialize
+    code_maker = CodeMaker.new
+    @colors = code_maker.colors
+  end
 
-    def guess_output(guess, string='Your')
-      array = [1,2,3,4,5,6]
-      output_colors = []
-      guess = guess.split('')
-      print "#{string} guess was "
-      guess.each do |item|
-        if array.include? (item.to_i)
-          print @colors[item.to_i - 1] + ' '
-        end
-      end
-      puts ''
+  def guess_output(guess, string = 'Your')
+    array = [1, 2, 3, 4, 5, 6]
+    guess = guess.split('')
+    print "#{string} guess was "
+    guess.each do |item|
+      print @colors[item.to_i - 1] + ' ' if array.include? item.to_i
     end
+    puts ''
+  end
 end
